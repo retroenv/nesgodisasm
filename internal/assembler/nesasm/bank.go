04c7f6c0f1ad9a75f310574691d8e176af4f6f25
cornelk <github@codejuggle.dj>
2023-08-25 11:07:20 -0600

    nesasm: support bank switch inside an instruction

--- internal/assembler/nesasm/bank.go
+++ internal/assembler/nesasm/bank.go
@@ -7,13 +7,14 @@ import (
 	"github.com/retroenv/nesgodisasm/internal/program"
 )
 
+const bankSize = 0x2000
+
 // addBankSelectors adds bank selectors to every 0x2000 byte offsets, as required
 // by nesasm to avoid the error "Bank overflow, offset > $1FFF".
 func addBankSelectors(codeBaseAddress int, prg []*program.PRGBank) int {
 	counter := 0
 	bankNumber := 0
 	bankAddress := codeBaseAddress
-	bankSize := 0x2000
 	bankSwitch := true
 
 	for _, bank := range prg {
@@ -21,9 +22,7 @@ func addBankSelectors(codeBaseAddress int, prg []*program.PRGBank) int {
 
 		for {
 			if bankSwitch { // if switch was carried over after last bank was filled
-				bank.PRG[index].WriteCallback = writeBankSelector(bankAddress, bankNumber)
-				bankAddress += bankSize
-				bankNumber++
+				setBankSelector(bank.PRG, index, &bankAddress, &bankNumber)
 				bankSwitch = false
 			}
 
@@ -43,9 +42,7 @@ func addBankSelectors(codeBaseAddress int, prg []*program.PRGBank) int {
 				break
 			}
 
-			bank.PRG[index+bankSpaceLeft].WriteCallback = writeBankSelector(bankAddress, bankNumber)
-			bankAddress += bankSize
-			bankNumber++
+			setBankSelector(bank.PRG, index+bankSpaceLeft, &bankAddress, &bankNumber)
 
 			index += bankSpaceLeft
 			counter += bankSpaceLeft
@@ -53,6 +50,37 @@ func addBankSelectors(codeBaseAddress int, prg []*program.PRGBank) int {
 	}
 
 	return bankNumber
+}
+
+func setBankSelector(prg []program.Offset, index int, bankAddress, bankNumber *int) {
+	offsetInfo := &prg[index]
+
+	// handle bank switches in the middle of an instruction by converting it to data bytes
+	if offsetInfo.IsType(program.CodeOffset) && len(offsetInfo.OpcodeBytes) == 0 {
+		// look backwards for instruction start
+		instructionStartIndex := index - 1
+		for offsetInfo = &prg[instructionStartIndex]; len(offsetInfo.OpcodeBytes) == 0; {
+			instructionStartIndex--
+			offsetInfo = &prg[instructionStartIndex]
+		}
+
+		offsetInfo.Comment = fmt.Sprintf("bank switch in instruction detected: %s %s",
+			offsetInfo.Comment, offsetInfo.Code)
+		data := offsetInfo.OpcodeBytes
+
+		for i := 0; i < len(data); i++ {
+			offsetInfo = &prg[instructionStartIndex+i]
+			offsetInfo.OpcodeBytes = data[i : i+1]
+			offsetInfo.ClearType(program.CodeOffset)
+			offsetInfo.SetType(program.CodeAsData | program.DataOffset)
+		}
+		offsetInfo = &prg[index]
+	}
+
+	offsetInfo.WriteCallback = writeBankSelector(*bankAddress, *bankNumber)
+
+	*bankAddress += bankSize
+	*bankNumber++
 }
 
 func writeBankSelector(bankAddress, bankNumber int) func(writer io.Writer) error {
